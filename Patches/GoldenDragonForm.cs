27640a364158b70c9669d7a56fb886c75812f82a
Balkoth-dev <webmastaman@yahoo.com>
2022-03-19 03:57:34 -0400

    Adjustments for gold dragon form's mental stat boosts.

--- Patches/GoldenDragonForm.cs
+++ Patches/GoldenDragonForm.cs
@@ -62,21 +62,31 @@ namespace WOTR_PATH_OF_BLING.Patches
                     return;
                 }
                 var dragonFormBuff = BlueprintTool.Get<BlueprintBuff>("dbe1d6ac18ad4eafb4f6d24e48eb12dc");
+                var dragonFormAbillity = BlueprintTool.Get<BlueprintAbility>("a0273cfaafe84f0b89a70b3580568ebc");
+                var dragonFormAbilityDescription = "You can assume the form of a Gold Dragon. You gain the following abilities: a +10 {g|Encyclopedia:Size}size{/g} " +
+                                                   "{g|Encyclopedia:Bonus}bonus{/g} to {g|Encyclopedia:Strength}Strength{/g}, a +8 size bonus to {g|Encyclopedia:Constitution}Constitution{/g}, " +
+                                                   "a +8 form natural armor bonus, Blindsense 60 feet. You also gain one bite ({g|Encyclopedia:Dice}2d8{/g}), two claws (2d6), two wing {g|Encyclopedia:Attack}attacks{/g} " +
+                                                   "(1d8), and one tail slap attack (2d6).\n You also gain a +2 Mythic bonus to all mental stats.";
+                dragonFormAbillity.m_Description = Helpers.CreateString(dragonFormAbillity + ".Description", dragonFormAbilityDescription);
+                dragonFormBuff.m_Description = Helpers.CreateString(dragonFormBuff + ".Description", dragonFormAbilityDescription);
 
                 dragonFormBuff.AddComponent<AddContextStatBonus>(c => {
                     c.Multiplier = 2;
                     c.Stat = Kingmaker.EntitySystem.Stats.StatType.Intelligence;
                     c.Descriptor = Kingmaker.Enums.ModifierDescriptor.Mythic;
+                    c.Value = 1;
                 });
                 dragonFormBuff.AddComponent<AddContextStatBonus>(c => {
                     c.Multiplier = 2;
                     c.Stat = Kingmaker.EntitySystem.Stats.StatType.Charisma;
                     c.Descriptor = Kingmaker.Enums.ModifierDescriptor.Mythic;
+                    c.Value = 1;
                 });
                 dragonFormBuff.AddComponent<AddContextStatBonus>(c => {
                     c.Multiplier = 2;
                     c.Stat = Kingmaker.EntitySystem.Stats.StatType.Wisdom;
                     c.Descriptor = Kingmaker.Enums.ModifierDescriptor.Mythic;
+                    c.Value = 1;
                 });
 
             }
