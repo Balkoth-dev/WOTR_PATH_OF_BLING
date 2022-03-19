a3504285e16dc463d7c3293ecfa5e583f025aab9
Balkoth-dev <webmastaman@yahoo.com>
2022-03-18 20:13:01 -0400

    Modified spell damage component and added it to feature.

---
+++ MechanicsChanges/GoldenDragonSpellDamage.cs
@@ -0,0 +1,91 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.EntitySystem;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem;
+using Kingmaker.RuleSystem.Rules.Damage;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.FactLogic;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+using Kingmaker.Utility;
+using Kingmaker.Utility.UnitDescription;
+using Owlcat.Runtime.Core.Utils;
+using System;
+using System.Collections.Generic;
+
+namespace WOTR_PATH_OF_BLING.MechanicsChanges
+{
+    [TypeId("a584568f53d2454a97a48123d6f7d78f")]
+    public class GoldenDragonSpellDamage : UnitFactComponentDelegate, IInitiatorRulebookHandler<RuleCalculateDamage>, IRulebookHandler<RuleCalculateDamage>, ISubscriber, IInitiatorRulebookSubscriber
+    {
+        public void OnEventAboutToTrigger(RuleCalculateDamage evt)
+        {
+            MechanicsContext context = evt.Reason.Context;
+            if((context != null ? context.SourceAbility : null) == null)
+            {
+                return;
+            }
+            if(!context.SourceAbility.IsSpell)
+            {
+                return;
+            }
+            foreach (BaseDamage baseDamage in evt.DamageBundle)
+            {
+                DiceType newDice = new DiceType();
+                if (base.Owner.Progression.MythicLevel == 8)
+                {
+                    newDice = DiceType.D6;
+                }
+                else if(base.Owner.Progression.MythicLevel == 9)
+                {
+                    newDice = DiceType.D8;
+                }
+                else if (base.Owner.Progression.MythicLevel == 10)
+                {
+                    newDice = DiceType.D10;
+                }
+                if (baseDamage.Dice.m_Dice < newDice)
+                {
+                    baseDamage.ReplaceDice(new DiceFormula(baseDamage.Dice.m_Rolls, newDice));
+                }
+                else
+                {
+                    int bonus = (1 * baseDamage.Dice.Rolls);
+                    baseDamage.AddModifier(bonus, base.Fact);
+                }
+                if (CheckVulnerability(evt.Target))
+                {
+                    baseDamage.m_Vulnerability = 1.5f;
+                }
+            }
+        }
+
+        static bool CheckVulnerability(UnitEntityData targetUnit)
+        {
+            foreach (EntityFact entityFact in targetUnit.Facts.List)
+            {
+                AddEnergyVulnerability component = entityFact.Blueprint.GetComponent<AddEnergyVulnerability>();
+                if (component != null)
+                {
+                    List<ContextActionDealDamage> actionDealDamageList = TempList.Get<ContextActionDealDamage>();
+                    foreach (ContextActionDealDamage actionDealDamage6 in actionDealDamageList)
+                    {
+                        if (actionDealDamage6.DamageType.Type == DamageType.Energy && actionDealDamage6.DamageType.Energy == component.Type)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public void OnEventDidTrigger(RuleCalculateDamage evt)
+        {
+
+        }
+
+    }
+}
---
+++ Patches/GoldenDragonSpellDamageFeatures.cs
@@ -0,0 +1,38 @@
+﻿using BlueprintCore.Utils;
+using HarmonyLib;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WOTR_PATH_OF_BLING;
+using WOTR_PATH_OF_BLING.MechanicsChanges;
+
+namespace WOTR_PATH_OF_RAGE.Patches
+{
+    [HarmonyPatch(typeof(BlueprintsCache), "Init")]
+    static class BlueprintsCache_Init_Patch
+    {
+        static bool Initialized;
+
+        static void Postfix()
+        {
+            if (Initialized) return;
+            Initialized = true;
+
+            PatchGoldenDragonSpellDamageFeature();
+
+            Main.Log("Patching Golden Dragon Spell Damage Features");
+        }
+
+        static void PatchGoldenDragonSpellDamageFeature()
+        {
+            var goldenDragonSpellDamageFeature = BlueprintTool.Get<BlueprintFeature>("a83dd922ad1441fd8224bd01a5948331");
+            goldenDragonSpellDamageFeature.RemoveComponents<AddSpellDiceBonusTrigger>();
+            goldenDragonSpellDamageFeature.AddComponent<GoldenDragonSpellDamage>();
+        }
+    }
+}
