09252cbe07513cf408444cc549b87aba4696e37f
Balkoth-dev <webmastaman@yahoo.com>
2022-12-17 16:21:26 -0500

    1.0.2

--- Patches/GoldenDragonSpellDamageFeatures.cs
+++ Patches/GoldenDragonSpellDamageFeatures.cs
@@ -42,16 +42,6 @@ namespace WOTR_PATH_OF_BLING.Patches
                 goldenDragonSpellDamageFeature.RemoveComponents<AddSpellDiceBonusTrigger>();
                 goldenDragonSpellDamageFeature.AddComponent<GoldenDragonSpellDamage>();
 
-                var newPerfectSoulDescription = "The Gold Dragon gains immunity to cold, fire, electricity and acid, energy drain and negative energy. " +
-                                                "All your {g|Encyclopedia:Damage}damage{/g} is now {g|Encyclopedia:Energy_Damage}holy damage{/g} and will " +
-                                                "deal additional damage if the target is {g|Encyclopedia:Vulnerable}vulnerable{/g} to any element.\n" +
-                                                "Any {g|Encyclopedia:Spell}spells{/g} dealing damage now change their dice to {g|Encyclopedia:Dice}d10{/g}, " +
-                                                "if it was not d10 or greater. If it was d10 or greater, the spell instead plus one {g|Encyclopedia:Bonus}bonus{/g} " +
-                                                "to damage per die.\nBonus to your highest ability score score increases to +8.";
-
-                var dragonLevel3Immunities = BlueprintTool.Get<BlueprintFeature>("f1631a20b6f14e58924a32c81da95840");
-                dragonLevel3Immunities.m_Description = Helpers.CreateString(dragonLevel3Immunities + ".Description", newPerfectSoulDescription);
-
                 var x = goldenDragonSpellDamageFeature.ToReference<BlueprintFeatureBaseReference>();
 
                 var goldenDragonProgression = BlueprintTool.Get<BlueprintProgression>("a6fbca43902c6194c947546e89af64bd");
